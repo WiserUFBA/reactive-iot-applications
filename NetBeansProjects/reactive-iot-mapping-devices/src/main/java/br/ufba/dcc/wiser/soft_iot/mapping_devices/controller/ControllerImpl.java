9433a3163324969b412a9b880281c71d493fa2bb
cleberlira <cleberlira@cleberlira-Inspiron-5566>
2019-11-21 17:07:39 -0300

    Reactive Microservices to map devices in a Gateway

--- NetBeansProjects/reactive-iot-mapping-devices/src/main/java/br/ufba/dcc/wiser/soft_iot/mapping_devices/controller/ControllerImpl.java
+++ NetBeansProjects/reactive-iot-mapping-devices/src/main/java/br/ufba/dcc/wiser/soft_iot/mapping_devices/controller/ControllerImpl.java
@@ -22,6 +22,7 @@ import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.spi.cluster.ClusterManager;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
 
 public class ControllerImpl extends AbstractVerticle implements Controller {
@@ -31,18 +32,17 @@ public class ControllerImpl extends AbstractVerticle implements Controller {
     private boolean debugModeValue;
     private String enderecoBarramento;
 
-    public void start(BundleContext context) {
+    @Override
+    public void start() {
         System.out.println("Starting mapping of connected devices...");
        
-        this.strJsonDevices = "[{id:libeliumSmartWater01,latitude:53.290411,longitude:-9.074406,sensors:[{id:TemperatureSensorPt-1000,type:WaterMeter,collection_time:30000,publishing_time: 60000}]}]";
-        
-        loadConnectedDevices(this.strJsonDevices, context);
+        loadConnectedDevices(this.strJsonDevices);
     }
 
-    private void loadConnectedDevices(String strDevices, BundleContext context) {
+    private void loadConnectedDevices(String strDevices) {
         List<Device> listDevices = new ArrayList<Device>();
         try {
-            System.out.println("JSON load:");
+            System.out.println("JSON load:" + strDevices);
             JSONArray jsonArrayDevices = new JSONArray(strDevices);
             for (int i = 0; i < jsonArrayDevices.length(); i++) {
                 JSONObject jsonDevice = jsonArrayDevices.getJSONObject(i);
@@ -69,15 +69,17 @@ public class ControllerImpl extends AbstractVerticle implements Controller {
         
         this.listDevices = listDevices;
 
-        addIoTMessageChannel(this.listDevices,context);
+        addIoTMessageChannel(this.listDevices);
 
     }
 
-    public void addIoTMessageChannel(List<Device> listDevices, BundleContext context) {
+    public void addIoTMessageChannel(List<Device> listDevices) {
 
         try{
         Gson json = new Gson();
         String devices = json.toJson(listDevices);
+
+        BundleContext context =  FrameworkUtil.getBundle(ControllerImpl.class).getBundleContext();    
 
         Vertx vetx = Vertx.vertx();
 
@@ -85,34 +87,17 @@ public class ControllerImpl extends AbstractVerticle implements Controller {
 
         EventBus eventBus = (EventBus) context.getService(eventBusRef);
 
-        vetx.setPeriodic(Util.timepublish, id -> {
+        //vetx.setPeriodic(Util.timepublish, id -> {
 
             eventBus.publish(Util.enderecoBarramento, devices);
             System.out.println("We now have a clustered event bus: " + eventBus);
-        });
+      //  });
         
         }catch(Exception v){
             v.printStackTrace();
         }
 
-        //     EventBus eventBus = Vertx.vertx().eventBus();
-//        try {
-//
-//            printlnDebug("We now have a clustered event bus: " + eventBus);
-//
-//            Gson json = new Gson();
-//
-//            String JSONObject = json.toJson(listDevices);
-//
-//            printlnDebug("enderecoBarramento " + enderecoBarramento);
-//
-//            eventBus.send(enderecoBarramento, "teste");
-//
-//        } catch (Exception c) {
-//            printlnDebug("erro " + c.getMessage());
-//            c.printStackTrace();
-//
-//        }
+    
     }
 
     @Override
